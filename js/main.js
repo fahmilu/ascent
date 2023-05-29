10558ad451ab3d1d8ff662048c1ca64439cef0c6
Fahmi <fahmi@catalyzecommunications.com>
2023-05-29 10:22:44 +0700

    update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@
 		ease: 'slide',
 		once: true
 	});
+	
+	const ListHiddenFeatured = document.getElementsByClassName("list-featured-hidde");
+	const seeMore = document.getElementsByClassName("see-more-featured");
 
+	const seeMoreHandler = (e) => {
+		e.preventDefault();
+		console.log('click');
+	};
 })()
 
